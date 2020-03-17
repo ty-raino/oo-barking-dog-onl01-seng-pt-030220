0b177b632791cbfc0f70c6e70418079e8002d7ab
Ty Raino <mstymetris@yahoo.com>
2020-03-17 09:33:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,15 +2,12 @@ class Dog
   def name=(dog_name)
    @dogs_name = (dog_name)
 
-   def name
-     @dogs_name
-
-
    def bark
      puts "woof!"
    end
- end
-   end
 end
 
- 
+ def name
+   @dogs_name
+  end
+ end
